eda1d5405243b488604dd615a006fb69a013424c
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-11-12 18:15:03 -0500

    contact page template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,44 +1,36 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from '../components/seo'
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 export default function Template({
-    data, // this prop will be injected by the GraphQL query below.
+	data // this prop will be injected by the GraphQL query below.
 }) {
-    const { markdownRemark } = data // data.markdownRemark holds your post data
-    const { frontmatter, html } = markdownRemark
-    return (
-        <Layout>
-            <SEO
-                title={frontmatter.title}
-                description={frontmatter.description} />
-            <div className="blog-post-container">
-                <div className="blog-post">
-                    <h1>{frontmatter.title}</h1>
-                    <h2>{frontmatter.date}</h2>
-                    <div
-                        className="blog-post-content"
-                        dangerouslySetInnerHTML={{ __html: html }}
-                    />
-                </div>
-            </div>
-        </Layout>
-
-    )
+	const { markdownRemark } = data; // data.markdownRemark holds your post data
+	const { frontmatter, html } = markdownRemark;
+	return (
+		<Layout>
+			<SEO title={frontmatter.title} description={frontmatter.description} />
+			<div className=''>
+				<div className=''>
+					<h1>{frontmatter.title}</h1>
+					<h2>{frontmatter.date}</h2>
+					<div className='' dangerouslySetInnerHTML={{ __html: html }} />
+				</div>
+			</div>
+		</Layout>
+	);
 }
 
 export const pageQuery = graphql`
-  query($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        slug
-        title
-        description
-        tags
-      }
-    }
-  }
-`
+	query($slug: String!) {
+		markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+			html
+			frontmatter {
+				slug
+				title
+				description
+			}
+		}
+	}
+`;
