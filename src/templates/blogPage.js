b8ee0330a53c1fa615b5e5816d30e6cb248aa5fe
felipe ospina calle <felipeospina21@hotmail.com>
2020-12-22 10:47:23 -0500

    delete Image component

--- src/templates/blogPage.js
+++ src/templates/blogPage.js
@@ -1,47 +1,48 @@
-import React from 'react';
+import React from "react"
 
-import Layout from '../components/Layout';
-import Image from '../components/Image';
-import SEO from '../components/SEO';
-import PostLink from '../components/PostLink';
-import { graphql } from 'gatsby';
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import PostLink from "../components/PostLink"
+import { graphql } from "gatsby"
 
-const BlogPageTemplate = ({ data: { allMarkdownRemark: { edges } } }) => {
+const BlogPageTemplate = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <h1>Blog Posts</h1>
+      {edges.map(edge => (
+        <PostLink key={edge.node.id} post={edge.node} />
+      ))}
+    </Layout>
+  )
+}
 
-	return (
-		<Layout>
-			<SEO title='Blog' />
-			<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-				<Image />
-			</div>
-			<h1>Blog Posts</h1>
-			{edges.map(edge => <PostLink key={edge.node.id} post={edge.node} />)}
-		</Layout>
-	);
-};
-
-export default BlogPageTemplate;
+export default BlogPageTemplate
 
 export const BlogPageQuery = graphql`
-	query {
-		allMarkdownRemark(
-			sort: { order: DESC, fields: [frontmatter___date] }
-			filter: { frontmatter: { templateKey: { eq: "blogPost" } } }
-		) {
-			edges {
-				node {
-					id
-					excerpt(pruneLength: 250)
-					frontmatter {
-						date(formatString: "MMMM DD, YYYY")
-						slug
-						title
-					}
-					fields {
-						slug
-					}
-				}
-			}
-		}
-	}
-`;
+  query {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blogPost" } } }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
